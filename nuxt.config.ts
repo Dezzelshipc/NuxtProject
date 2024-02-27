4692830ff8024e9ff8a3d4d26d94c03c488568ee
Dezzelshipc <foler126@gmail.com>
2024-02-27 19:56:32 +1000

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
+  experimental: {
+    payloadExtraction: true
+  },
 
   modules: ['@nuxtjs/google-fonts'],
 
