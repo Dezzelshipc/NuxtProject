1a244468f448a29b25f891fcb6e582108a5c8bec
Dezzelshipc <foler126@gmail.com>
2024-02-27 19:22:07 +1000

    Task 1
    Hello, world!

--- app.vue
+++ app.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    <NuxtWelcome />
+  <div class="center">
+    <h1 class="hw">
+      Hello, world!
+    </h1>
   </div>
 </template>
+
+<script>
+
+console.log("\nHello, console, I mean, world!\n")
+
+</script>
+
+<style>
+
+@import url(~/assets/styles/styles.css);
+
+</style>
