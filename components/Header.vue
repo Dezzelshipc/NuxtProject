61d81b5a29365fb2957450ed311425cd67f0533a
Dezzelshipc <foler126@gmail.com>
2024-03-10 15:18:30 +1000

    Task 2
    Header, Body, Footer, banners, text

---
+++ components/Header.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    Header
+  </header>
+</template>
+
+
+<style lang="scss" scoped>
+
+header {
+    padding: var(--padding-normal);
+    font-family: var(--font-title);
+    
+    font-size: 2rem;
+    font-weight: 600;
+
+    background-color: deepskyblue;
+}
+
+</style>
